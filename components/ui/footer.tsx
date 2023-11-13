1c2e371f862192ad0b1393824e2cae2e23ad9c4c
Lavynia de Oliveira <lavynia.oliveira@unifei.edu.br>
2023-11-13 18:51:08 -0300

    feat: tela das obras, mocks json e cards

---
+++ components/ui/footer.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+export default function Footer() {
+
+  return (
+    <header className='fixed w-full z-30 transition duration-300 ease-in-out bg-amber-950'>
+      <div className="max-w-6xl mx-auto px-5 sm:px-6">
+        <div className="flex items-center justify-between h-16 md:h-50">
+        <div className="shrink-0 mr-4">
+            </div>
+        </div>
+      </div>
+    </header>
+  )
+}
